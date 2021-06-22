cd7823b01bdce68e677a2ab587e656a0c8c5bd40
Mary78234 <wang11quanquan@gmail.com>
2021-06-22 23:46:36 +0200

    footer done

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,1 +1,85 @@
-<footer>footer</footer>
+<footer>
+  <section class="links">
+    <div class="container">
+
+      <!-- colonna 1 -->
+      <div>
+        <ul>
+          <li><a href="#">DC COMICS</a></li>
+          <li><a href="#">Characters</a></li>
+          <li><a href="#">Comics</a></li>
+          <li><a href="#">Movies</a></li>
+          <li><a href="#">TV</a></li>
+          <li><a href="#">Games</a></li>
+          <li><a href="#">Videos</a></li>
+          <li><a href="#">News</a></li>
+        </ul>
+
+        <ul>
+          <li><a href="#">SHOP</a></li>
+          <li><a href="#">Shop DC</a></li>
+          <li><a href="#">Shop DC Collectabilities</a></li>
+        </ul>
+
+      </div>
+      <!-- /colonna 1 -->
+
+      <!-- colonna 2 -->
+      <div>
+        <ul>
+          <li><a href="#">DC</a></li>
+          <li><a href="#">Terms Of Use</a></li>
+          <li><a href="#">Privacy policy(New)</a></li>
+          <li><a href="#">Ad Choices</a></li>
+          <li><a href="#">Advertising</a></li>
+          <li><a href="#">Jobs</a></li>
+          <li><a href="#">Subscriptions</a></li>
+          <li><a href="#">Talent Workshops</a></li>
+          <li><a href="#">CPSC Certificates</a></li>
+          <li><a href="#">Ratings</a></li>
+          <li><a href="#">Shop Help</a></li>
+          <li><a href="#">Contact Us</a></li>
+        </ul>
+      </div>
+      <!-- /colonna 2 -->
+
+      <!-- colonna 3 -->
+      <div>
+        <ul>
+          <li><a href="#">SITES</a></li>
+          <li><a href="#">DC</a></li>
+          <li><a href="#">MAD Magazine</a></li>
+          <li><a href="#">DC kids</a></li>
+          <li><a href="#">DC Universe</a></li>
+          <li><a href="#">DC Power Visa</a></li>
+        </ul>
+      </div>
+      <!-- /colonna 3 -->
+
+    </div>
+  </section>
+
+  <section class="social">
+    <div class="container">
+
+      <div>
+        <button>SIGN-UP NOW!</button>
+      </div>
+
+      <div>
+        <ul>
+          <li>FOLLOW US</li>
+          <li><a href="#"><img src="../img/footer-facebook.png" alt="facebook"></a></li>
+          <li><a href="#"><img src="../img/footer-twitter.png" alt="twitter"></a></li>
+          <li><a href="#"><img src="../img/footer-youtube.png" alt="youtube"></a></li>
+          <li><a href="#"><img src="../img/footer-pinterest.png" alt="pinterest"></a></li>
+          <li><a href="#"><img src="../img/footer-periscope.png" alt="eriscope"></a></li>
+        </ul>
+      </div>
+
+    </div>
+  
+  </section>
+
+
+</footer>
